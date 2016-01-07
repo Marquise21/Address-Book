591ac7f816cf1572e0a977416b773e07ae7d6b1f
Spectre21 <howard.marquise21@gmail.com>
2016-01-06 19:55:35 -0600

    Update Address Book.py

--- Address Book.py
+++ Address Book.py
@@ -1,5 +1,19 @@
 address_book = []
 
+def refresh_book():
+    global address_book
+    address_book = sorted(address_book, key=lambda person: person.name)
+
+def display_content():
+    refresh_book()
+    for p in address_book:
+        if(type(p).__name__ == "person"):
+            print(p.name)
+            print(" address:"+p.address)
+            print(" number:"+p.number)
+            print(" email:"+p.email)
+            print("")
+
 class person:
     def __init__(self, name,address,number,email):
         self.name = name
@@ -19,14 +33,22 @@ class person:
     def set_email(self, email):
         self.email = email
 
+def create_person():
+    name = input('Please enter a name:')
+    address = input('Please enter an address:')
+    number = input('Please enter a phond number:')
+    email = input('Please enter an email:')
+    p = person(name,address,number,email)
+    return p
+    
 
-a = person("Marquise","Chicago","1234567","Mcago@email.com")
+a = person("Marquise","Chicago","1234567","Mmadjahwqxncsajaajgo@Eemail.com")
 address_book.append(a)
 
-for p in address_book:
-    if(type(p).__name__ == "person"):
-        print(p.name)
-        print(" address:"+p.address)
-        print(" number:"+p.number)
-        print(" email:"+p.email)
-        print("")
+a = person("Danial","Chicago","Unknown","Unknown")
+address_book.append(a)
+
+display_content()
+
+
+
